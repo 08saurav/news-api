c63808ef1a036428b046b07d2f5cccff93b87ccc
saurabh.bhartiya <saurabh.bhartiya@juspay.in>
2023-07-30 01:23:10 +0530

    added for cors errors

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ const cache = new NodeCache({ stdTTL: 60 * 5 }); // Cache TTL set to 5 minutes
 // Middleware to parse JSON data in request body
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://127.0.0.1:60681",
-  })
-);
+// const corsOptions = {
+//   origin: "*",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Serve the frontend static files
 app.use(express.static("public"));
 
